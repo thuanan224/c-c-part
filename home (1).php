3c53c540f161950bc9757d3b19d5748b6cbc64e4
thuanan224 <levanthuan2706@gmail.com>
2024-08-22 00:45:56 +0700

    add file

---
+++ home (1).php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<h1>Hello</h1>
+
+<?php
+echo '<p style="text-align: center; color: green;">Xin chào TMU</p>';
+echo '<button style="background-color:red;">click</button>';
+echo '<a target="_blank" href ="https://www.youtube.com/">Youtube</a> '
+?>
+    
+</body>
+</html>
